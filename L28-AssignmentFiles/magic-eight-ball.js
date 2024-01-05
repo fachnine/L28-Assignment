02a18e4579512efde5ce3392410f8ae70853239b
fachnine <fachnine@my.waketech.edu>
2024-01-04 23:27:16 -0500

    put javascript code

---
+++ L28-AssignmentFiles/magic-eight-ball.js
@@ -0,0 +1,11 @@
+<script>
+const answers = ["Yes","No","Maybe","Ask again later","Definitely","Never","Cannot predict now"];
+let function = displayAnswer(let index = Math.floor(Math.random() * answers.length));
+displayAnswer ("circle");
+element.addEventListener ("click", function);
+if getElementbyID("question") ;
+alert ("Please type a question");
+else displayAnswer (function);
+element.addEventListener ("click", reset);
+displayAnswer ("none");
+</script>
